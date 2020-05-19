4a86479733dc455b641aecb9965fe7da2b6a4262
megal <wmmegal@gmail.com>
2020-05-19 12:26:33 +0300

    main complete

--- build/js/common.js
+++ build/js/common.js
@@ -1,6 +1,72 @@
 jQuery(document).ready(function ($) {
 
     /**
+     * Меню
+     */
+
+    let menuA = $('#nav a');
+    menuA.each(function () {
+        if ($(this).next('ul').length) {
+            $(this).append(' <i class="icon-arr"></i>')
+        }
+    });
+
+    $(document).on('click', '#nav a', function (e) {
+        let $this = $(this);
+        let thisParent = $this.closest('li'),
+            ul = thisParent.find('> ul');
+        if (ul.length > 0) {
+            e.preventDefault();
+
+            if (thisParent.hasClass('active')) {
+                thisParent.removeClass('active')
+            } else {
+                thisParent.addClass('active').siblings().removeClass('active');
+            }
+
+            // if ($(window).width() < 1200) {
+            //     if (!ul.find('.btn-menu-back').length) {
+            //         ul.prepend('<li><button class="btn-menu-back"><i class="icon-arr"></i>' + $this.text() + '</button></li>');
+            //     }
+            // }
+        }
+    });
+
+    $(document).on('click', function (e) {
+        hideElement($(e.target), '#nav', $('#nav li'));
+    });
+
+    $(document).on('click', '.btn-menu-back', function (e) {
+        $(this).closest('ul').closest('li').removeClass('active');
+    });
+
+    $('.btn-menu').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('active').next().slideToggle();
+        $('#header').toggleClass('show-menu');
+    });
+
+    /**
+     * Поиск
+     */
+    let quickSearch = $('#quick-search');
+    $('.btn-show-search').on('click', function (e) {
+        quickSearch.addClass('active').find('input').focus();
+    });
+
+    quickSearch.find('input').on('blur', function (e) {
+        setTimeout(function () {
+            quickSearch.removeClass('active');
+            $(this).closest('form').find('.search-results').removeClass('active');
+        }, 1)
+    });
+
+    quickSearch.find('input').on('input', function (e) {
+        $(this).closest('form').find('.search-results').addClass('active');
+    });
+
+
+    /**
      * Слайдер на главной
      */
     $('#single-slider').slick({
@@ -34,7 +100,17 @@ jQuery(document).ready(function ($) {
         slidesToShow: 3,
         slidesToScroll: 1,
         infinite: false,
-        appendArrows: $('.slider-products-arrows')
+        appendArrows: $('.slider-products-arrows'),
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            },
+        ]
     });
 
 
@@ -53,6 +129,30 @@ jQuery(document).ready(function ($) {
                 {
                     breakpoint: 1300,
                     settings: {
+                        arrows: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 3,
+                        arrows: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        arrows: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1,
                         arrows: false,
                         dots: true
                     }
@@ -80,5 +180,14 @@ jQuery(document).ready(function ($) {
     });
 
 });
+
+/**
+ * Спрятать элемент по клику из вне
+ */
+function hideElement(el, parent, elHide) {
+    if (!el.closest(parent).length) {
+        elHide.removeClass('active');
+    }
+}
 
 //# sourceMappingURL=common.js.map
